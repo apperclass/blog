3dd6b5a9e1b54e26e305d1a9a0d038b6f0ec9b68
oriodesign <oriodesign@gmail.com>
2015-01-09 14:49:25 +0100

    [SEO] improve seo

--- wp-content/themes/apperclass/header.php
+++ wp-content/themes/apperclass/header.php
@@ -11,7 +11,18 @@ if(isset($_GET['s'])){
 <html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
-    <title><?php wp_title(); ?></title>
+    <title><?php echo bloginfo('name'); wp_title(); ?></title>
+    <meta name="description" content="<?php if(is_singular()){
+        /** @var \WP_Post $post */
+        echo $post->post_excerpt;
+    }elseif(is_category()){
+        $categories = get_the_category();
+        $category_id = $categories[0]->cat_ID;
+        echo strip_tags(category_description($category_id));
+    }else{
+        echo bloginfo('description');
+    }?>">
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="profile" href="http://gmpg.org/xfn/11" />
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
@@ -33,16 +44,28 @@ if(isset($_GET['s'])){
     <div id="category-overlay"></div>
     <div class="main-header">
         <div class="row">
+
+
+            <?php if(is_category()):?>
             <h1 class="blog-title-wrapper">
-                <a class="blog-logo" href="<?php echo get_home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/white-logo.png" alt="Apperclass Blog"></a>
-
-                <?php if(is_category()):?>
-                    <a class="blog-section-title" href="#"><?php single_cat_title(); ?></a>
-                <?php endif; ?>
-
+                <a class="blog-logo" href="http://apperclass.com"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/white-logo.png" alt="Apperclass Blog"></a>
+                <a class="blog-section-title" href="<?php echo get_home_url(); ?>">BLOG</a>
+                <span class="blog-section-separator">&gt; </span><a class="blog-section-category-title" href="#"><?php single_cat_title(); ?></a>
                 <div class="clearfix"></div>
             </h1>
-
+            <?php elseif(is_singular()):?>
+                <div class="blog-title-wrapper">
+                    <a class="blog-logo" href="http://apperclass.com"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/white-logo.png" alt="Apperclass Blog"></a>
+                    <a class="blog-section-title" href="<?php echo get_home_url(); ?>">BLOG</a>
+                    <div class="clearfix"></div>
+                </div>
+            <?php else: ?>
+                <h1 class="blog-title-wrapper">
+                    <a class="blog-logo" href="http://apperclass.com"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/white-logo.png" alt="Apperclass Blog"></a>
+                    <a class="blog-section-title" href="<?php echo get_home_url(); ?>">BLOG</a> &gt;
+                    <div class="clearfix"></div>
+                </h1>
+            <?php endif; ?>
                 <form class="search-form">
                     <span class="form-icon"><span class="icon-search"></span></span><input class="<?php if($searchIsOpen){ echo "focused"; }?>" name="s" type="search" value="<?php echo $s; ?>" >
                 </form>
